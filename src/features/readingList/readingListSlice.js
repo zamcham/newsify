c688e9be113b27061f69bff2f2968ea1d113122c
Cristian Zambrano <criszamcham@gmail.com>
2023-10-04 21:09:42 -0700

    Add readingList Slice

---
+++ src/features/readingList/readingListSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    newsList: [],
+    total: 0,
+};
+
+export const readingListSlice = createSlice({
+    name: 'readingList',
+    initialState,
+    reducers: {
+        addToReadingList: (state, action) => {
+            state.newsList.push(action.payload);
+        },
+        removeFromReadingList: (state, action) => {
+            state.newsList = state.newsList.filter((news) => news.id !== action.payload.id);
+        },
+    },
+});
+
+export const { addToReadingList, removeFromReadingList } = readingListSlice.actions;
+
+export default readingListSlice.reducer;
