0493d54d12226aa63fbc6fe86a692b86419f5562
Cristian Zambrano <criszamcham@gmail.com>
2023-10-05 20:30:21 -0700

    Add news component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,16 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Categories = ({ categories }) => {
+const categories = [
+    'Politics',
+    'Technology',
+    'Business',
+    'Entertainment',
+    'Health',
+    'Science',
+    'Sports',
+    'World',
+    'Travel',
+    'Food',
+    'Fashion',
+    'Environment',
+    'Education',
+    'Finance',
+    'Gaming',
+    'Music',
+];
+  
+
+function Categories({ onSelectCategory }) {
+    const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  
+    const handleCategoryClick = (category) => {
+      setSelectedCategory(category);
+      onSelectCategory(category);
+    };
+  
     return (
-        <div className="container category-panel">
-            <h2>Categories</h2>
-            <ul>
-                {categories.map((category, index) => (
-                    <li key={index}>{category}</li>
-                ))}
-            </ul>
-        </div>
+      <div className="sidebar">
+        <h2>Categories</h2>
+        <ul>
+          {categories.map((category) => (
+            <li
+              key={category}
+              className={selectedCategory === category ? 'active' : ''}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}
+            </li>
+          ))}
+        </ul>
+      </div>
     );
-};
+  }
 
-export default Sidebar;
+export default Categories;
