bc1dbbe842d1e38edd3ddca6c0de45a7c6eda923
Cristian Zambrano <criszamcham@gmail.com>
2023-10-18 11:33:40 -0700

    Add base ternary expression to make articles show up

--- src/components/ReadingList.js
+++ src/components/ReadingList.js
@@ -1,7 +1,16 @@
+import React from "react";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
+
 const ReadingList = () => {
+    const { newsList, total } = useSelector((store) => store.readingList);
+
     return (
-        <div>
-            <h1>Reading List</h1>
+        <div className="main">
+            {total === 0 ? (
+                <p>You have not added any article yet</p>
+            ) : (
+                <p>Your articles will show up here</p>
+                )}
         </div>
     );
 }
