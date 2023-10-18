b30ada4157ab15f11faaf3a1be6007b6394d494b
Cristian Zambrano <criszamcham@gmail.com>
2023-10-18 11:44:35 -0700

    Update card base structure

--- src/components/ReadingListCard.js
+++ src/components/ReadingListCard.js
@@ -4,7 +4,10 @@ const ReadingListCard = ({ article }) => {
             <div className="card-body">
                 <h5 className="card-title">{article.title}</h5>
                 <p className="card-text">{article.description}</p>
+            </div>
+            <div className="card-actions">
                 <a href={article.url} className="btn btn-primary">Read</a>
+                <a href="#" className="btn btn-danger">Remove</a>
             </div>
         </div>
     );
