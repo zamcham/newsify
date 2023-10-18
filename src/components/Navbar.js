7db61f45cba0591506ebd53fc5ce50261f977a72
Cristian Zambrano <criszamcham@gmail.com>
2023-10-18 11:10:49 -0700

    Remove key text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,6 @@ import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faHouse, } from '@fortawesome/free-solid-svg-icons';
 
-// 0360eaf44b2547a3845fce34e7d44ed7 (news API key)
-
 const Navbar = () => {
     const readingListTotal = useSelector((store) => store.readingList.total);
     return (
