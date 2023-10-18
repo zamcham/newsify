26e9ed1fea7c3fe2338fe9fe1edbcf37999e9779
Cristian Zambrano <criszamcham@gmail.com>
2023-10-18 15:11:32 -0700

    Updated props on News component

--- src/components/News.js
+++ src/components/News.js
@@ -9,10 +9,9 @@ function News({ category }) {
   useEffect(() => {
     const currentDate = new Date();
     currentDate.setDate(currentDate.getDate() - 1);
-    const formattedDate = currentDate.toISOString().split("T")[0];
     const apiKey = process.env.REACT_APP_NEWS_API_KEY;
 
-    const apiUrl = `https://newsapi.org/v2/everything?q=${category}&apiKey=${apiKey}&from=${formattedDate}&to=${formattedDate}`;
+    const apiUrl = `https://api.thenewsapi.com/v1/news/all?&api_token=${apiKey}&language=en&limit=&categories=${category}`;
 
     async function fetchNews() {
       try {
@@ -21,18 +20,7 @@ function News({ category }) {
           throw new Error(`Request failed with status: ${response.status}`);
         }
         const data = await response.json();
-        const englishArticles = data.articles.filter((article) => {
-          try {
-            const detectedLanguageCode = franc(article.title);
-            return detectedLanguageCode === "eng";
-          } catch (error) {
-            console.error(
-              `Error detecting language for article: ${article.title}`,
-            );
-            return false;
-          }
-        });
-        setArticles(englishArticles);
+        setArticles(data.data);
       } catch (error) {
         console.error(error);
       }
@@ -56,14 +44,13 @@ function News({ category }) {
       <div className="news-container" ref={newsContainerRef}>
         <ul>
           {articles.map((article) => (
-            <li key={article.url}>
+            <li key={article.uuid}>
               <NewsCard
                 articleObject={article}
-                image={article.urlToImage}
+                image={article.image_url}
                 title={article.title}
-                source={article.source.name}
-                author={article.author}
-                date={article.publishedAt}
+                source={article.source}
+                date={article.published_at}
                 articleLink={article.url}
               />
             </li>
