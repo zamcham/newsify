d2cccb3e3c1b385406800a4f1b873f1a88ff344e
Cristian Zambrano <criszamcham@gmail.com>
2023-10-18 11:27:07 -0700

    Add reset to home page button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,21 @@ import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faHouse, } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => {
+const Navbar = ({ setShowReadingList }) => {
     const readingListTotal = useSelector((store) => store.readingList.total);
+
+    const handleHomeClick = () => {
+        setShowReadingList(false);
+    };
+
     return (
             <nav>
                 <div className="logo">
                     <h1>Newsify</h1>
                 </div>
                 <ul>
-                    <li className='active'><a href="#"><FontAwesomeIcon icon={faHouse} className='menu-icon'/> Home</a></li>
-                    <li><a href="#">Reading List</a></li>
+                    <li className='active'><a onClick={handleHomeClick}><FontAwesomeIcon icon={faHouse} className='menu-icon'/> Home</a></li>
+                    <li><a onClick={() => setShowReadingList(true)}>Reading List</a></li>
                 </ul>
                 <div className="actions">
                 <div className="reading-list-icon">
