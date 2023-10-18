e822aaba5592470c5d7d2abc308f516b54c09c9b
Cristian Zambrano <criszamcham@gmail.com>
2023-10-17 21:32:29 -0700

    Refactor slice to add and remove articles from reading list

--- src/features/readingList/readingListSlice.js
+++ src/features/readingList/readingListSlice.js
@@ -10,14 +10,16 @@ export const readingListSlice = createSlice({
     initialState,
     reducers: {
         toggleNewAddition: (state, action) => {
-            const existingIndex = state.newsList.findIndex((news) => news.id === action.payload.id);
+            const existingIndex = state.newsList.findIndex((news) => news.source.name === action.payload.source.name);
             if (existingIndex === -1) {
+                console.log('Adding new article to reading list');
                 return {
                     ...state,
                     newsList: [...state.newsList, action.payload],
                     total: state.total + 1,
                 };
             } else {
+                console.log('Removing article from reading list');
                 return {
                     ...state,
                     newsList: state.newsList.filter((news) => news.id !== action.payload.id),
